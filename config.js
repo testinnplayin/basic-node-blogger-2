bcabff526e75db95bdb28ee8827608db2d3c2609
Rachelle <archaurore@gmail.com>
2016-12-22 20:58:59 -0500

    added other functions to server.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://localhost/basic-node-blogger-2';
+exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://localhost/node-blogger';
 exports.PORT = process.env.PORT || 8080;
--- server.js
+++ server.js
@@ -19,14 +19,16 @@ const {BlogPost} = require('./models');
 
 //CRUD functions
 
-app.get('/blog-posts', (req, res) => {
+//reading
+
+app.get('/posts', (req, res) => {
 	BlogPost
 		.find()
 		.exec()
-		.then(blogposts => {
+		.then(blogs => {
 			res.json({
-				blogposts: blogposts.map(function(blogpost) {
-					blogpost.apiRepr();
+				blogs: blogs.map(function(blog) {
+					blog.apiRepr();
 				})
 			});
 		})
@@ -34,6 +36,93 @@ app.get('/blog-posts', (req, res) => {
 			console.error(err);
 			res.status(500).json({ message: 'Internal server error' });
 		});
+});
+
+app.get('/posts/:id', (req, res) => {
+	BlogPost
+		.findById(req.params.id)
+		.exec()
+		.then(blog => res.json(blog.apiRepr()))
+		.catch(err => {
+			console.error(err);
+			res.status(500).json({ message: 'Internal server error'});
+		});
+});
+
+//create
+
+app.post('/posts', (req, res) => {
+	const requiredFields = ['title', 'content', 'author'];
+	requiredFields.forEach(field => {
+		if (!(field in req.body && req.body[field])) {
+			return res.status(400).json({ message: `Please enter a valid ${field}`});
+		}
+	});
+
+	BlogPost
+		.create({
+			title: req.body.title,
+			content: req.body.content,
+			author: req.body.author
+		})
+		.then(blog => {
+			res.status(201).json(blog.apiRepr());
+		})
+		.catch(err => {
+			console.error(err);
+			res.status(500).json({ message: 'Internal server error' });
+		});
+});
+
+//update
+
+app.put('/posts/:id', (req, res) => {
+	if (!(req.params.id && req.body.id && (req.params.id === req.body.id))) {
+		const message = (
+			`Request path id (${req.params.id}) and request body id` +
+			`(${req.body.id}) must match`
+		);
+		console.error(message);
+		res.status(400).json({ message: message});
+	}
+
+	const updateData = {};
+	const updateFields = ['title', 'content', 'author'];
+
+	updateFields.forEach(field => {
+		if (field in req.body) {
+			updateData[field] = req.body[field];
+		}
+	});
+
+	BlogPost
+		.findByIdAndUpdate(req.params.id, {$set: updateData}, {nes: true})
+		.exec()
+		.then(updatedPost => {
+			res.status(201).json(updatedPost.apiRepr());
+		})
+		.catch(err => {
+			res.status(500).json({message: 'Internal server error'});
+		});
+});
+
+//delete
+
+app.delete('/posts/:id', (req, res) => {
+	Restaurant
+		.findByIdAndRemove(req.params.id)
+		.exec()
+		.then(() => {
+			res.status(204).json({ message: 'successfully deleted' });
+		})
+		.catch(err => {
+			console.error(err);
+			res.status(500).json({ message: 'Internal server error' });
+		});
+});
+
+app.use('*', function(req, res) {
+	res.status(404).json({ message: 'Not Found'});
 });
 
 
